3b8a1366322901929bfccbc7cd2c27d1a276e4df
bmstefanski <bstefanskicom@gmail.com>
2020-08-28 21:37:30 +0200

    Add prettier & replace stubs with my data

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,14 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `Bart Stefanski | Software Engineer`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: `Bart Stefanski`,
+      summary: `who builds things for the web.`,
     },
-    description: `A starter blog demonstrating what Gatsby can do.`,
-    siteUrl: `https://gatsby-starter-blog-demo.netlify.app/`,
+    description: `A self-taught web software engineer portfolio and blog about node.js, react stack, clean code & architecture.`,
+    siteUrl: `https://bstefanski.com/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `bmstefanski`,
     },
   },
   plugins: [
@@ -60,11 +60,11 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Gatsby Starter Blog`,
-        short_name: `GatsbyJS`,
+        name: `Bart Stefanski Blog`,
+        short_name: `bmstefanski`,
         start_url: `/`,
         background_color: `#ffffff`,
-        theme_color: `#663399`,
+        theme_color: `#2E3440`,
         display: `minimal-ui`,
         icon: `content/assets/gatsby-icon.png`,
       },
