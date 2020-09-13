2d9013208f7367198caca99ea8dd255e7f22038f
Bart Stefanski <contact@bstefanski.com>
2020-09-13 21:50:43 +0200

    Setup google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -58,7 +58,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-156493134-1`,
       },
     },
     `gatsby-plugin-feed`,
