d87b8b2ce5d844eed3ec008908e5b05ecff8ee04
Bart Stefanski <contact@bstefanski.com>
2021-07-23 23:00:28 +0200

    move OpenGraphHead's hardcoded data to env vars

---
+++ env.js
@@ -0,0 +1,6 @@
+export const EnvVars = {
+  OG_IMAGES_URL: 'https://dev.bstefanski.com/og-images/',
+  URL: 'https://bstefanski.com/',
+  FACEBOOK_PROFILE: 'https://www.facebook.com/bmstefanski',
+  TWITTER_PROFILE: '"@bmstefanski"',
+}
--- views/SingleArticlePage/OpenGraphHead.js
+++ views/SingleArticlePage/OpenGraphHead.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import Head from 'next/head'
+import { EnvVars } from 'env'
 
 export default function OpenGraphHead(props) {
   const { slug, title, description, date, tags } = props
 
-  const currentUrl = `https://bstefanski.com/${slug}`
-  // TODO:
-  const ogImageUrl = `https://dev.bstefanski.com/og-images/${slug}.png`
+  const currentUrl = EnvVars.URL + slug
+  const ogImageUrl = EnvVars.URL + `${slug}.png`
 
   return (
     <Head>
@@ -19,11 +19,11 @@ export default function OpenGraphHead(props) {
       <meta property="og:image:alt" content={description} />
       <meta property="og:locale" content="en_US" />
       <meta property="article:published_time" content={new Date(date)} />
-      <meta property="article:author" content="https://www.facebook.com/bmstefanski" />
+      <meta property="article:author" content={EnvVars.FACEBOOK_PROFILE} />
       <meta property="article:section" content={tags} />
       <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:creator" content="@bmstefanski" />
-      <meta name="twitter:site" content="@bmstefanski" />
+      <meta name="twitter:creator" content={EnvVars.TWITTER_PROFILE} />
+      <meta name="twitter:site" content={EnvVars.TWITTER_PROFILE} />
       <meta name="twitter:title" content={title} />
       <meta name="twitter:description" content={description} />
       <meta name="twitter:image" content={ogImageUrl} />
