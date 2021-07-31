edf074b648c1d9407e398fbb70f866096055c0c3
Bart Stefanski <contact@bstefanski.com>
2021-07-31 16:18:42 +0200

    fix typo in post & add title to 404 & 500 pages

--- pages/404.js
+++ pages/404.js
@@ -1,10 +1,16 @@
 import Container from 'components/Container'
 import React from 'react'
+import Head from 'next/head'
 
 export default function Custom404() {
   return (
-    <Container>
-      <h1>404 - Page Not Found</h1>
-    </Container>
+    <>
+      <Head>
+        <title>Not found | bstefanski.com</title>
+      </Head>
+      <Container>
+        <h1>404 - Page Not Found</h1>
+      </Container>
+    </>
   )
 }
--- pages/500.js
+++ pages/500.js
@@ -1,11 +1,18 @@
 import Container from 'components/Container'
 import React from 'react'
 import styled from 'styled-components'
+import Head from 'next/head'
 
 export default function Custom500() {
   return (
-    <Container>
-      <h1>500 - Server-side error occurred</h1>
-    </Container>
+    <>
+      <Head>
+        <title>Ooops XD | bstefanski.com</title>
+      </Head>
+      <Container>
+        <h2>Oooops XD</h2>
+        <h1>500 - Server-side error occurred</h1>
+      </Container>
+    </>
   )
 }
