f6af16586a156b5856213ac8638257d3aa387123
Bart Stefanski <contact@bstefanski.com>
2021-07-31 16:22:11 +0200

    dev -> prod

--- env.js
+++ env.js
@@ -1,5 +1,5 @@
 export const EnvVars = {
-  OG_IMAGES_URL: 'https://dev.bstefanski.com/og-images/',
+  OG_IMAGES_URL: 'https://bstefanski.com/og-images/',
   URL: 'https://bstefanski.com/',
   FACEBOOK_PROFILE: 'https://www.facebook.com/bmstefanski',
   TWITTER_PROFILE: '"@bmstefanski"',
