d9c501f5d5c04376e00192bc1913d00abd4fbb39
Bart Stefanski <contact@bstefanski.com>
2021-11-23 20:50:56 +0100

    generate og images for snippets

--- scripts/generate-og-images.js
+++ scripts/generate-og-images.js
@@ -6,20 +6,30 @@ const path = require('path')
 
 ;(async function () {
   const postsDirectory = path.join(process.cwd(), 'posts')
-  const allPosts = fs.readdirSync(postsDirectory).map(normalizePostName)
+  const snippetsDirectory = path.join(process.cwd(), 'snippets')
+
+  const allPosts = getAllEntries(postsDirectory)
+  const allSnippets = getAllEntries(snippetsDirectory)
 
   // prettier-ignore
-  Promise.all(allPosts
-    .map(getSinglePost)
+  Promise.all([...allPosts, ...allSnippets]
+    .map(([slug, directory]) => getSinglePost(slug, directory))
     .map(fetchImageBlob))
     .then(writeFilesToPublicDirectory)
+
+  function getAllEntries(directory) {
+    return fs
+      .readdirSync(directory)
+      .map(normalizePostName)
+      .map((slug) => [slug, directory])
+  }
 
   function normalizePostName(postName) {
     return postName.replace('.mdx', '')
   }
 
-  function getSinglePost(slug) {
-    const filePath = path.join(postsDirectory, slug + '.mdx')
+  function getSinglePost(slug, directory) {
+    const filePath = path.join(directory, slug + '.mdx')
     const contents = fs.readFileSync(filePath, 'utf8')
     const { data: meta } = matter(contents)
     return [slug, meta]
