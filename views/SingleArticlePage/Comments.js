7c511a60b9d2034f18c694dca8f53a16abb35cb0
Bart Stefanski <contact@bstefanski.com>
2022-07-10 19:56:50 +0200

    bring back comments

--- views/SingleArticlePage/Comments.js
+++ views/SingleArticlePage/Comments.js
@@ -2,26 +2,21 @@
 // source https://github.com/utterance/utterances/issues/161#issue-443109697
 
 import { useEffect } from 'react'
-import { useThemeContext } from 'contexts/theme.context'
 
 export default function Comments() {
-  const { theme } = useThemeContext()
-  const widgetTheme = theme === 'light' ? 'github-light' : 'github-dark'
-
   useEffect(() => {
-    if (theme) {
-      let script = document.createElement('script')
-      let anchor = document.getElementById('inject-comments-for-uterances')
+    let script = document.createElement('script')
+    let anchor = document.getElementById('inject-comments-for-uterances')
 
-      script.setAttribute('src', 'https://utteranc.es/client.js')
-      script.setAttribute('crossorigin', 'anonymous')
-      script.setAttribute('defer', true)
-      script.setAttribute('repo', 'bmstefanski/personal-site')
-      script.setAttribute('issue-term', 'pathname')
-      script.setAttribute('theme', widgetTheme)
-      anchor.appendChild(script)
-    }
-  }, [theme, widgetTheme])
+    script.setAttribute('src', 'https://utteranc.es/client.js')
+    script.setAttribute('crossorigin', 'anonymous')
+    script.setAttribute('defer', true)
+    script.setAttribute('repo', 'bmstefanski/personal-site')
+    script.setAttribute('issue-term', 'pathname')
+    script.setAttribute('theme', 'github-dark')
+    script.setAttribute('loading', 'lazy')
+    anchor.appendChild(script)
+  }, [])
 
-  return <div id="inject-comments-for-uterances" key={widgetTheme} style={{ width: '100%' }}></div>
+  return <div id="inject-comments-for-uterances" style={{ width: '100%' }}></div>
 }
