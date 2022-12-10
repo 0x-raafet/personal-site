a5b6cc6abb591426ae50787e567c9eae1bbaef01
Bart Stefanski <contact@bstefanski.com>
2022-12-10 18:16:08 +0100

    remove security headers

--- next.config.js
+++ next.config.js
@@ -68,30 +68,5 @@ module.exports = withLinaria(
         { source: '/widget.css', destination: 'https://made-by-widget.vercel.app/main.css' },
       ]
     },
-    async headers() {
-      return [
-        {
-          source: '/(.*)',
-          headers: [
-            {
-              key: 'X-DNS-Prefetch-Control',
-              value: 'on',
-            },
-            {
-              key: 'X-XSS-Protection',
-              value: '1; mode=block',
-            },
-            {
-              key: 'X-Frame-Options',
-              value: 'SAMEORIGIN',
-            },
-            {
-              key: 'Referrer-Policy',
-              value: 'origin-when-cross-origin',
-            },
-          ],
-        },
-      ]
-    },
   }),
 )
