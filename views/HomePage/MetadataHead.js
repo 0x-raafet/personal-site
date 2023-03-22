e34a5b74faeaa73dc0118631d3e7a4582c0c3cf8
Bart Stefanski <contact@bstefanski.com>
2023-03-22 22:31:47 +0100

    yandex and yahoo verif

--- views/HomePage/MetadataHead.js
+++ views/HomePage/MetadataHead.js
@@ -13,6 +13,8 @@ export default function MetadataHead(props) {
       <meta property="og:title" content="Bart Stefański" />
       <meta property="og:description" content={description} />
       <meta property="og:site_name" content="Bart Stefański" />
+      <meta name="msvalidate.01" content="569F112AF9AAA5B36792C8D9CD150514" />
+      <meta name="yandex-verification" content="2205d0fb8859f110" />
     </Head>
   )
 }
